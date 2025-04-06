39f41fc0457e4eba699c3ebdc28918c3e6d06c29
Netik Viacheslav <slava5582@gmail.com>
2025-04-06 23:03:29 +0200

    Added Question Page and small loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MainLayout } from './components/MainLayout';
 import { HomePage } from './pages/HomePage';
+import { QuestionPage } from './pages/QuestionPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/forbidden" element={<div>forbidden !!!</div>} />
           <Route path="/addquestion" element={<div>add question</div>} />
-          <Route path="/question/:id" element={<div>QUESTION PAGE</div>} />
+          <Route path="/question/:id" element={<QuestionPage />} />
 
           <Route path="*" element={<NotFoundPage />} />
           <Route />
