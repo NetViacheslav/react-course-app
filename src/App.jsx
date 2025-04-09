96cc2a212cd0186b56ef6851b45f1a34d1707f27
Netik Viacheslav <slava5582@gmail.com>
2025-04-09 23:11:57 +0200

    Add lazy components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MainLayout } from './components/MainLayout';
 import { HomePage } from './pages/HomePage';
 import { QuestionPage } from './pages/QuestionPage';
-import { AddQuestionPage } from './pages/AddQuestionPage';
+import { AddQuestionPageLazy } from './pages/AddQuestionPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/forbidden" element={<div>forbidden !!!</div>} />
-          <Route path="/addquestion" element={<AddQuestionPage />} />
+          <Route path="/addquestion" element={<AddQuestionPageLazy />} />
           <Route path="/question/:id" element={<QuestionPage />} />
 
           <Route path="*" element={<NotFoundPage />} />
--- src/pages/AddQuestionPage/AddQuestionPage.jsx
+++ src/pages/AddQuestionPage/AddQuestionPage.jsx
@@ -41,7 +41,7 @@ const createCardAction = async (_prevState, formData) => {
   }
 };
 
-export const AddQuestionPage = () => {
+const AddQuestionPage = () => {
   const [formState, formAction, isPending] = useActionState(createCardAction, {
     clearForm: false,
   });
@@ -129,3 +129,5 @@ export const AddQuestionPage = () => {
     </>
   );
 };
+
+export default AddQuestionPage;
